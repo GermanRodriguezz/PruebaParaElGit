dfda61f87b2413a6c45e250e94c0ebbc1b0998f2
German Rodriguez <gerdepo04mail.com>
2025-03-31 11:57:25 -0300

    se arreglo el error si se ingresaban caracteres de mas

--- src/funciones/ValidacionUsuario.py
+++ src/funciones/ValidacionUsuario.py
@@ -29,9 +29,13 @@ def validarUsuario(usuario) :
 def validarUsuario15(user,fecha,resto):
     if (len(user) < 16) : # verifico si tengo los 15 caracteres del usuario
         cant = (30 - (len(user) + len(fecha))) # aca me quedo con cuantos caracteres me faltan agregar para que se cree el codigo de descuento
+        print('se rellenara el codigo de descuento con :',cant , 'de caracteres aleatorios')
         codigo = ""
         nombre = user.upper()
         for i in range(cant) :                      # aca voy a repetir x veces
             codigo = codigo + random.choice(resto)  # por cada iteracion me traigo un caracter de la variable resto que tiene cargada los nums y letras mayusculas
         codigo = f"{nombre}-{fecha}-{codigo}"
+        return codigo
+    else :
+        codigo = "se superaron los 15 caracteres validos para la generacion del codigo!"
     return codigo
