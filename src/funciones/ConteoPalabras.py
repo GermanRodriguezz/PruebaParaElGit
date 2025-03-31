dda61ae739805384579068505d19f579b2b7e3fd
German Rodriguez <gerdepo04mail.com>
2025-03-31 10:12:38 -0300

    Se agrego Ejercicio 6

---
+++ src/funciones/ConteoPalabras.py
@@ -0,0 +1,31 @@
+def comparar(palabra,lista):       #esta funcion recorre cada lista de mi tupla de descripciones y verifica si se encuentra alguna palabra pasada por parametro
+    for elem in lista :
+        if palabra in elem.lower().split() : #paso a minuscula la palabra de la lista y las separo con el .split() para que compare con la palabra exacta
+            return True
+    else :
+        return False
+
+def conteo(descrip):  
+    m = 0
+    e = 0
+    c = 0
+    for lista in descrip :      #esta funcion le manda la palabra a buscar a la lista y si retorna true va acumulara en el valor de cada clave 1+
+        print('recorrera la lista ', lista)
+        if comparar('música',lista) :
+            m += 1
+            print('sumo m')
+
+        if comparar('entretenimiento',lista) :
+            e += 1
+            print('sumo e')
+
+        if comparar('charla',lista):
+            c += 1
+            print('sumo c')
+    menciones = {'musica' : m,
+                'entretenimiento' : e,
+                'charla' : c}
+    print('Menciones de musica :' ,menciones['musica'] )
+    print('Menciones de entretenimiento :' ,menciones['entretenimiento'])
+    print('Menciones de charla : ',menciones['charla'])
+        
