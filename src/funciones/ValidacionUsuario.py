cce5779aa97131adf1e4b4b045ea41948b754393
German Rodriguez <gerdepo04mail.com>
2025-03-31 11:48:13 -0300

    se agrego ejercicio 7

--- src/funciones/ValidacionUsuario.py
+++ src/funciones/ValidacionUsuario.py
@@ -1,4 +1,5 @@
 import string
+import random
 
 digitos = string.digits
 Mayusculas = string.ascii_uppercase
@@ -23,4 +24,14 @@ def validarUsuario(usuario) :
                 puede = False
                 break
         return puede
-        
+    
+
+def validarUsuario15(user,fecha,resto):
+    if (len(user) < 16) : # verifico si tengo los 15 caracteres del usuario
+        cant = (30 - (len(user) + len(fecha))) # aca me quedo con cuantos caracteres me faltan agregar para que se cree el codigo de descuento
+        codigo = ""
+        nombre = user.upper()
+        for i in range(cant) :                      # aca voy a repetir x veces
+            codigo = codigo + random.choice(resto)  # por cada iteracion me traigo un caracter de la variable resto que tiene cargada los nums y letras mayusculas
+        codigo = f"{nombre}-{fecha}-{codigo}"
+    return codigo
