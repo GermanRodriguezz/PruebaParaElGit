902a395eedc0d7320c89d17a413fab261e9b13c7
German Rodriguez <gerdepo04mail.com>
2025-04-03 10:19:54 -0300

    se modifico punto 8

--- src/funciones/VerificacionAnagrama.py
+++ src/funciones/VerificacionAnagrama.py
@@ -1,12 +1,7 @@
 def anagrama(primerP,segundaP):
-    if len(primerP) == len(segundaP) : # 1ro - si tienen la misma cantidad de caracteres sera lo primero que chekeo
-        print('la palabra es ',primerP)
-        for i in range(len(segundaP)) :     # 2do - tengo que ver si las letras de la 2da palabra se encuentran en la primer palabra,para eso recorro
-            letra = segundaP[i]
-            print(letra)
-            if not letra in primerP : # 3ro - si no se encuentra una letra de la 2da palabra dentro de la primera, corto
-                return False
-                break
+    if len(primerP) != len(segundaP) : # 1ro - si tienen la misma cantidad de caracteres sera lo primero que chekeo
+        return False 
+    if sorted(primerP) == sorted(segundaP):
         return True
-    else :
-        return False                         # si termino el for es porque encontro todas las letras de la 2da palabra en la primer palabra
+    else:
+        return False
