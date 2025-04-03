f71ff6c1bba08448af6e518b86ba9b6208e5fbb6
German Rodriguez <gerdepo04mail.com>
2025-04-03 11:36:09 -0300

    se medificaron comentarios

--- src/funciones/vocales.py
+++ src/funciones/vocales.py
@@ -1,7 +1,7 @@
-def verificar_vocal(renglon,lista) : # recibo la linea de palabras del texto      # aca
+def verificar_vocal(renglon,lista) : # recibo la linea de palabras del texto      
     palabrasDelRenglon = renglon.split()
     if len(palabrasDelRenglon) > 1 : # si tengo mas de una palabra,evaluo la segunda
-        palabra = palabrasDelRenglon[1]
+        palabra = palabrasDelRenglon[1] # tomo la segunda palabra del renglon
         if  palabra.startswith(lista) :
             return True
     return False # en caso de que no tengo segunda palabra la linea retorna Flases
